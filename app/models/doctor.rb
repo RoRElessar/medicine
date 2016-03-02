6799f7a87ace0eb9f1a21f7731a3f8080adfdec5
RoRElessar <palantiri@ukr.net>
2016-03-02 20:52:15 +0200

    added validations in user, doctor and company models, also added errors messages

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,9 @@
 class Doctor < ActiveRecord::Base
   belongs_to :company
+
+  validates_presence_of :name, :surname, :second_name, :specialization, :photo, :email, :password
+  validates :name, :surname, :second_name, length: {in: 2..20}
+
   after_create :encrypt_password
 
   def encrypt_password
