e227bc0b9009f4641eda5763b501ba9ae726b2c4
Serhii-Danovsky <jipebed@gmail.com>
2016-04-13 13:16:58 +0300

    add current_user.record  add Занято Ваш запис

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
-
+  def current_doctor
+    current_doctor ||= Doctor.find(session[:doctor_id]) if session[:doctor_id]
+  end
 
   protected
 
